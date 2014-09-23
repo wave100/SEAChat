d4b8b6a6583be109cff9ab5cee6a5a36c4eebae0
TheGuy191919 <evan_1998@sina.com>
2014-09-22 21:45:48 -0400

    Work On Protocol Event

--- SEAChat/src/seachat/net/protocol/ProtocolEventHandler.java
+++ SEAChat/src/seachat/net/protocol/ProtocolEventHandler.java
@@ -5,10 +5,38 @@
  */
 package seachat.net.protocol;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  *
  * @author evan__000
  */
 public class ProtocolEventHandler {
     
+    Map<Integer, ProtocolEvent> mapOfListener = new LinkedHashMap();
+    
+    public ProtocolEventHandler(){
+        
+    }
+    
+    public void regesterListner(ProtocolEvent protocolEvent){
+        this.mapOfListener.put(-1, protocolEvent);
+    }
+    
+    public void regesterListner(ProtocolEvent protocolEvent, int listenFor){
+        this.mapOfListener.put(listenFor, protocolEvent);
+    }
+    
+    public void removeListner(ProtocolEvent protocolEvent){
+        for(int a = 0; a < this.mapOfListener.size(); a++){
+            if(this.mapOfListener.get(a).equals(protocolEvent)){
+                this.mapOfListener.remove(a);
+            }
+        }
+    }
+    
+    protected void messageGotten(Protocol protocol){
+        
+    }
 }
